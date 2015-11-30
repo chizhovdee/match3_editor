19935bc4d546d9ea45f7c90af17dcb82ed20595a
Dmitry Chizhov <chizhov.dee@gmail.com>
2015-11-30 17:47:25 +0500

    Added creating level

---
+++ app/controllers/levels_controller.rb
@@ -0,0 +1,20 @@
+class LevelsController < ApplicationController
+  def new
+    last_level = Dir[Rails.root.join('public', 'levels', '*')].sort_by{|f| File.basename(f)}.last
+    new_level = ("%03d" % (File.basename(last_level).to_i + 1)).scan(/\d{3}/).join("/")
+
+    result = FileUtils.mkdir_p(Rails.root.join('public', 'levels', "#{ new_level }"))
+
+    redirect_to edit_level_path new_level
+
+
+    # time = Time.zone.now.to_i
+    # File.open(Rails.root.join(result, "#{time}.json"), File::CREAT|File::RDWR) do |file|
+    #   file.write(MultiJson.encode({datetime: time}))
+    # end
+  end
+
+  def edit
+
+  end
+end
